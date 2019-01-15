043451009b4602091b55a274f8f87ed572342778
Hussar <34105208+HussarHussar@users.noreply.github.com>
2019-01-14 21:29:04 -0500

    Add files via upload

---
+++ WorldControl.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldControl : MonoBehaviour
+{
+	public bool inCombat;
+	public bool stageOver;
+	public float tileSizeX = 384;
+	public float scrollSpeed;
+	private Transform backDrop;
+	private Transform floor;
+	private Vector3 startPosition;
+	private WorldProperties worldProps;
+	public GameObject heroIn;
+	private GameObject h1;
+	private GameObject h2;
+	private GameObject h3;
+	private GameObject back;
+
+	public WorldControl(HeroQueue q){
+		//Spawn appropriate units with correct weapon sprites	
+	
+	}
+    // Start is called before the first frame update
+    void Start()
+    {
+	    //Setup the backdrop (All possible backdrops will later be sorted and stored under the empty object 'backdrops' to be added
+	   	back = GameObject.Find("a1z1s1backDrop");
+			back.SetActive(true); 
+
+
+	    	//Retrieves Hero slots and WorldProperties
+		h1 = GameObject.Find("HeroInstance1");
+		h2 = GameObject.Find("HeroInstance2");
+		h3 = GameObject.Find("HeroInstance3");
+	    	worldProps = GetComponent<WorldProperties>();
+
+
+	    //Instantiate Heros by placing unit data from world properties slot into HeroInstance slot and activating instance;
+	    if (GetComponent<WorldProperties>().h1 != null){
+		    (h1.GetComponent(typeof(StateProperties)) as StateProperties).data = worldProps.h1;
+		   h1.SetActive(true);
+		//Instantiate(heroIn, transform.Find("Slot1Spawner").transform.position, transform.Find("Slot1Spawner").localRotation, transform);
+	    }
+
+	   if (GetComponent<WorldProperties>().h2 != null){
+		    (h2.GetComponent(typeof(StateProperties)) as StateProperties).data = worldProps.h2;
+		   h2.SetActive(true);
+	    }
+
+	   if (GetComponent<WorldProperties>().h3 != null){
+		    (h3.GetComponent(typeof(StateProperties)) as StateProperties).data = worldProps.h3;
+		   h3.SetActive(true);
+	    }
+
+
+
+
+      backDrop = transform.Find("BackDrop"); 
+      floor = transform.Find("Floor"); 
+      startPosition = backDrop.position;
+      tileSizeX = 18.5f;
+      scrollSpeed = 5f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       //Checks if there are enemies. If not, alive units run forward in correct formation; 
+       //Backdrop movement
+      float newPosition = Mathf.Repeat(Time.time * scrollSpeed, tileSizeX);
+      backDrop.transform.position = startPosition + Vector3.left * newPosition;
+      
+    }
+
+   
+}
---
+++ WorldProperties.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldProperties : MonoBehaviour
+{
+	// World Properties will instanciate Background sprites/object and floor object,
+	// and blocks area 
+	
+	private int area;
+	private int zone;
+	private int stage;
+	public bool inCombat;
+	public UnitData h1;
+	public UnitData h2;
+	public UnitData h3;
+
+
+	//Gets
+	
+	public int getArea(){
+		return area;	
+	}
+	
+	public int getZone(){
+		return zone;
+	}
+
+	public int getStage(){
+		return stage;
+	}
+	
+
+	//Sets
+
+	public void setArea(int n){
+		area = n;
+	}
+	public void setStage(int n){
+		stage = n;
+	}
+	public void setZone(int n){
+		zone = n;
+	}	
+	public WorldProperties(int a, int z, int s){
+		area = a;	
+		zone = z;	
+		stage = s;	
+		// if a == z == s == 1 then start scene for first level
+	
+	
+	}
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       // HeroInstance.GetComponent<Rigidbody2D>().velocity = new Vector2(3,
+	 //      GetComponent<Rigidbody2D>().velocity.y);
+    
+ 
+    }
+}
